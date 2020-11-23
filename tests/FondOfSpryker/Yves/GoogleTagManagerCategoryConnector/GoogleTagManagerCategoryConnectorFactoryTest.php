526340d5340c28a1c8a50ee1e4e3a6ba44e3a865
Pascal Fischer <p-fischer@gmx.net>
2020-11-23 18:04:16 +0100

    fix factory unit test

--- tests/FondOfSpryker/Yves/GoogleTagManagerCategoryConnector/GoogleTagManagerCategoryConnectorFactoryTest.php
+++ tests/FondOfSpryker/Yves/GoogleTagManagerCategoryConnector/GoogleTagManagerCategoryConnectorFactoryTest.php
@@ -3,13 +3,31 @@
 namespace FondOfSpryker\Yves\GoogleTagManagerCategoryConnector;
 
 use FondOfSpryker\Yves\GoogleTagManagerCategoryConnector\Model\GoogleTagManagerCategoryModelInterface;
+use Spryker\Yves\Kernel\Container;
 
 class GoogleTagManagerCategoryConnectorFactoryTest
 {
     /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\Spryker\Yves\Kernel\Container
+     */
+    protected $containerMock;
+
+    /**
      * @var \FondOfSpryker\Yves\GoogleTagManagerCategoryConnector\GoogleTagManagerCategoryConnectorFactory
      */
     protected $factory;
+
+    /**
+     * @return void
+     */
+    protected function _before(): void {
+        $this->containerMock = $this->getMockBuilder(Container::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->factory = new GoogleTagManagerCategoryConnectorFactory();
+        $this->factory->setContainer($this->containerMock);
+    }
 
     /**
      * @return void
